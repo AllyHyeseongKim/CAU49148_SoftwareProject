c41342c0c2bd69718bb335d1a891b270d5979a36
HyesyeongKim <ally3k@icloud.com>
2019-05-07 11:50:04 +0900

    Update link

--- project4/ParaStackNoLimit.java
+++ project4/ParaStackNoLimit.java
@@ -6,6 +6,7 @@ public class ParaStackNoLimit<E> extends ParaStack<E> {
 			super.size = super.size * 2;
 			E[] temp = (E[]) new Object[super.size];
 			System.arraycopy(super.list, 0, temp, 0, super.list.length);
+			super.list = temp;
 		}
 		super.push(element);
 	}
